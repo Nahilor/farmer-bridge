c451ff81ea28f22a90a1ac6de31e54477a8b230b
Nebil Sufeyan <124250593+nebils181@users.noreply.github.com>
2026-05-20 18:12:51 +0300

    Integrate authRoutes for user authentication
    Added authentication routes for login and registration.

--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB();
+const authRoutes = require("./src/routes/authRoutes.js")
+
+app.use("/api/auth", authRoutes); // this is expected to add the register and login route like this: 
+// /api/auth/login & /api/auth/register with POST method
+
 
 
 app.listen(PORT, () => {
