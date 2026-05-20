dcb6a4153c3f720a3426f9f6b82d57e54e820b51
Nebil Sufeyan <124250593+nebils181@users.noreply.github.com>
2026-05-20 19:35:21 +0300

    Update authRoutes.js

--- Server/src/routes/authRoutes.js
+++ Server/src/routes/authRoutes.js
@@ -6,7 +6,7 @@ const { register } = require("../services/signService");
 const router = express.Router();
 
 // register route should be created here
-// router.post('/register', register);
+router.post('/register', register);
 
 
 // Login route
