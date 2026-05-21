c39af5cf0e5211ed4495642ffd3927039cee64d8
Nebil Sufeyan <124250593+nebils181@users.noreply.github.com>
2026-05-21 11:27:24 +0300

    Update authRoutes.js

--- Server/src/routes/authRoutes.js
+++ Server/src/routes/authRoutes.js
@@ -1,15 +1,9 @@
 const express = require("express");
-// register controller should be imported here 
-const { login } = require("../controllers/authController");
-const { register } = require("../services/signService");
+const { login, register } = require("../controllers/authController");
 
 const router = express.Router();
 
-// register route should be created here
 router.post('/register', register);
-
-
-// Login route
 router.post("/login", login);
 
 module.exports = router;
