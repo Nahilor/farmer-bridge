e1f23ccb25fad9670cb695223e4f978aaf794b82
Nebil Sufeyan <124250593+nebils181@users.noreply.github.com>
2026-06-05 17:49:01 +0300

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,28 +1,14 @@
-const express = require('express');
-const dotenv = require('dotenv');
+require('dotenv').config();
 const connectDB = require('./src/config/db');
-const authRoutes = require("./src/routes/authRoutes.js")
-const adminRoutes = require("./src/routes/adminRoutes.js")
-const farmerRoutes = require("./src/routes/farmerRoutes.js");
-const farmerOrderRoutes = require("./src/routes/farmerOrderRoutes");
-const retailerProfileRoutes = require("./src/routes/retailerProfileRoutes");
-const retailerFarmerRoutes = require("./src/routes/retailerFarmerRoutes");
-const retailerOrderRoutes = require("./src/routes/retailerOrderRoutes");
-dotenv.config();
+const app = require('./app');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-connectDB();
-app.use(express.json());
-app.use("/api/auth", authRoutes); // this is expected to add the register and login route like this: 
-// /api/auth/login & /api/auth/register with POST method
-app.use("/api/admin", adminRoutes)
-app.use("/api/farmer", farmerRoutes);
-app.use("/api/farmer", farmerOrderRoutes);
-app.use("/api/retailer/profile", retailerProfileRoutes);
-app.use("/api/retailer/farmers", retailerFarmerRoutes);
-app.use("/api/retailer/orders", retailerOrderRoutes);
 
+// Connect to database
+connectDB();
+
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(` Server running on http://localhost:${PORT}`);
+  console.log(` API : http://localhost:${PORT}/api/working`);
 });
