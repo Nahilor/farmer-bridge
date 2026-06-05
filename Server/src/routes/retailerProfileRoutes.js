79e5d36e7d89b0bbdec3d84d8c4749e93b350a7f
Nebil Sufeyan <124250593+nebils181@users.noreply.github.com>
2026-06-05 17:13:19 +0300

    Update retailerProfileRoutes.js

--- Server/src/routes/retailerProfileRoutes.js
+++ Server/src/routes/retailerProfileRoutes.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const protect = require("../middlewares/authMiddleware");
+const { getRetailerProfile, updateRetailerProfile } = require("../controllers/retailerProfileController");
 
+const router = express.Router();
+
+router.get("/", protect, getRetailerProfile);
+router.patch("/", protect, updateRetailerProfile);
+
+module.exports = router;
