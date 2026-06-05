d57207afc9e86dd83d48199b017c58f16597727d
Nebil Sufeyan <124250593+nebils181@users.noreply.github.com>
2026-06-05 17:13:54 +0300

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,9 @@ const authRoutes = require("./src/routes/authRoutes.js")
 const adminRoutes = require("./src/routes/adminRoutes.js")
 const farmerRoutes = require("./src/routes/farmerRoutes.js");
 const farmerOrderRoutes = require("./src/routes/farmerOrderRoutes");
+const retailerProfileRoutes = require("./src/routes/retailerProfileRoutes");
+const retailerFarmerRoutes = require("./src/routes/retailerFarmerRoutes");
+const retailerOrderRoutes = require("./src/routes/retailerOrderRoutes");
 dotenv.config();
 
 const app = express();
@@ -16,6 +19,9 @@ app.use("/api/auth", authRoutes); // this is expected to add the register and lo
 app.use("/api/admin", adminRoutes)
 app.use("/api/farmer", farmerRoutes);
 app.use("/api/farmer", farmerOrderRoutes);
+app.use("/api/retailer/profile", retailerProfileRoutes);
+app.use("/api/retailer/farmers", retailerFarmerRoutes);
+app.use("/api/retailer/orders", retailerOrderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
