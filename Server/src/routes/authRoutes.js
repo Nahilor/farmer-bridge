f7fbb24d8816f2325a8a786814be7d3320036802
Nebil Sufeyan <124250593+nebils181@users.noreply.github.com>
2026-06-05 17:09:36 +0300

    Update authRoutes.js

--- Server/src/routes/authRoutes.js
+++ Server/src/routes/authRoutes.js
@@ -5,7 +5,7 @@ const { login, register } = require("../controllers/authController");
 const router = express.Router();
 
 router.post('/register', register);
-router.get("/login", login);
+router.post("/login", login);
 
 
 
